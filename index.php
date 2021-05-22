585cb605ee30bea0b6c51fce1c6ada381e25ed04
Argha Roy <37504332+royargha53@users.noreply.github.com>
2021-05-22 14:33:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,19 +7,7 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->result->parameters->text;
-	
-	
-
-	 if($text == '')
-
-	{
-		
-		$speech = $text;
-	}
- else{
-	 $speech = 'dd';
- }
+	$speech = $json;
  
 	$response = new \stdClass();
 	$response->speech = "$speech";
